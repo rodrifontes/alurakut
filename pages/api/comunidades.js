f4492844a99362ec16668de92514599b240b6535
Rodrigo Fontes Cruz <rodrifontes@gmail.com>
2021-07-17 13:18:25 -0300

    Correcao Aula 4

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -2,13 +2,12 @@ import { SiteClient } from 'datocms-client';
 
 export default async function recebedorDeRequests(request, response) {
     if(request.method === 'POST') {
-        const TOKEN = '6b50d4bcb1b7f021e7064cd270c42b';
-        console.log('TOKEN: ' + TOKEN);
+        const TOKEN = 'ae3664eda2b0bbd19b1e7c70a14f67';
         const client = new SiteClient(TOKEN);
         
         // Validar os dados, antes de sair cadastrando
         const registroCriado = await client.items.create({
-            itemType: "836335", // ID do Model de "Communities" criado pelo Dato
+            itemType: "974749", // ID do Model de "Communities" criado pelo Dato
             ...request.body,
             // title: "Comunidade de Teste",
             // imageUrl: "https://github.com/omariosouto.png",
